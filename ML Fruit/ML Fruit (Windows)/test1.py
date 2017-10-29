141169bb1c5dabcd02448bda1c004a5d1ec337f7
Nate G <15306437+Nate-Gaudreault@users.noreply.github.com>
2017-10-28 22:13:51 -0700

    Update test1.py

--- ML Fruit/ML Fruit (Windows)/test1.py
+++ ML Fruit/ML Fruit (Windows)/test1.py
@@ -1,5 +1,7 @@
 from sklearn import tree
-features = [[140, 1], [130, 1], [150, 0], [170, "0"]]
+#1 = smooth & 0 = bumpy
+features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+# 1 = orange & 0 = apple
 labels = [0, 0, 1, 1]
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
