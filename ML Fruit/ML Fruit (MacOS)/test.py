04ed8147d643d33a8ae10af6992a8a05d0b80f06
Nate G <15306437+Nate-Gaudreault@users.noreply.github.com>
2017-10-28 18:33:02 -0700

    Add files via upload

---
+++ ML Fruit/ML Fruit (MacOS)/test.py
@@ -0,0 +1,8 @@
+from sklearn import tree
+#1=smooth and 0 =bumpy
+features = [[140, 1], [130, 1], [150, 0],[170, 0]]
+labels = [0, 0, 1, 1]
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+#1=orange and 0=apple
+print clf.predict([[150, 0]])
